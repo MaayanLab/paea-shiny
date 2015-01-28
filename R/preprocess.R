248017923550bcd270056b36eab9a012a32e17a0
zero323 <matthew.szymkiewicz@gmail.com>
2015-01-28 04:24:20 +0100

    Remove nested stri_clean

--- R/preprocess.R
+++ R/preprocess.R
@@ -26,15 +26,14 @@ download_data <- function(url = 'https://localhost/microtask.csv') {
 #' @return tbl_dt
 #'
 preprocess_data <- function(df) {
-    clean_stri <- function(x) stri_replace_all_fixed(x, '&Acirc;&nbsp;', '')
-    
     tbl_dt(df) %>% rename(
         geo_accession=V1, control=V2, treatment=V3,
         gene=V4, perturbation=V5, species=V6, tissue_cell_line=V7,
         upregulated=V8, downregulated=V9, user=V10, datetime=V11, id=V12
     ) %>% 
     # Remove weird characters    
-    mutate_each(funs(clean_stri), control:treatment) %>%
+    mutate(control = stri_replace_all_fixed(control, '&Acirc;&nbsp;', '')) %>% 
+    mutate(treatment = stri_replace_all_fixed(treatment, '&Acirc;&nbsp;', '')) %>%
     # Remove [ACCN]
     mutate(geo_accession = factor(
         stri_trim_both(stri_replace_all_fixed(geo_accession, '[ACCN]', '')))) %>%
