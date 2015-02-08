a84419aa61011db5cdb411cd8a428bed1ffd6928
zero323 <matthew.szymkiewicz@gmail.com>
2015-02-08 18:10:57 +0100

    Add paea_analysis_dispatch

--- R/paea.R
+++ R/paea.R
@@ -20,6 +20,7 @@ prepare_paea_results <- function(paea_pvalues, data_description) {
 #' @param gmtfile see GeoDE::chdirAnalysis
 #' @param gammas see GeoDE::chdirAnalysis
 #' @param casesensitive see GeoDE::chdirAnalysis
+#' @return paea results
 #'
 
 paea_analysis_wrapper <- function(chdirresults, gmtfile, gammas = c(1), casesensitive = FALSE){
@@ -27,4 +28,35 @@ paea_analysis_wrapper <- function(chdirresults, gmtfile, gammas = c(1), casesens
     paea <- GeoDE::PAEAAnalysis(chdirresults, gmtfile, gammas, casesensitive, showprogress=TRUE)
     dev.off()
     paea
+}
+
+
+#' PAEAAnalysis dispatch function. Should handle separating chdirresults into up and down
+#' and in future some filtering steps
+#' 
+#' @param chdirresults see GeoDE::chdirAnalysis
+#' @param gmtfile see GeoDE::chdirAnalysis
+#' @param gammas see GeoDE::chdirAnalysis
+#' @param casesensitive see GeoDE::chdirAnalysis
+#' @return list with up and down fields
+#'
+paea_analysis_dispatch <- function(chdirresults, gmtfile, gammas = c(1), casesensitive = FALSE){
+    # Split chdir results into up and down
+    chdirresults_mask <- chdirresults[[1]][[1]][, 1] < 0
+    chdirresults_up <-  list(list(chdirresults[[1]][[1]][!chdirresults_mask, , drop=FALSE]))
+    chdirresults_down <- list(list(chdirresults[[1]][[1]][chdirresults_mask, , drop=FALSE]))
+    
+    # Split gmt file into up and down
+    gmtfile_mask <- unlist(lapply(gmtfile, function(x) { stringi::stri_endswith_fixed(x[1], '_downregulated') }))
+    gmtfile_up <- gmtfile[!gmtfile_mask]
+    gmtfile_down <- gmtfile[gmtfile_mask]
+    
+    list(
+        up =  if(length(gmtfile_up) > 0 & length(chdirresults_up) > 0) {
+            paea_analysis_wrapper(chdirresults_up, gmtfile_up,  gammas, casesensitive)
+        },
+        down =  if(length(gmtfile_down) > 0 & length(chdirresults_down) > 0) {
+            paea_analysis_wrapper(chdirresults_down, gmtfile_down,  gammas, casesensitive)
+        }
+    )
 }
