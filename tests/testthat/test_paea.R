f58bafeb6ec5912d749d058bacbfe480387431bd
zero323 <matthew.szymkiewicz@gmail.com>
2015-02-08 19:26:46 +0100

    Add empty tests for paea_analysis_wrapper and paea_analysis_dispatch

--- tests/testthat/test_paea.R
+++ tests/testthat/test_paea.R
@@ -1,5 +1,15 @@
 library(testthat)
 
 testthat::test_that('prepare_paea_results', {
-    #TODO
+
+})
+
+
+testthat::test_that('paea_analysis_wrapper', {
+
+})
+
+
+testthat::test_that('paea_analysis_dispatch', {
+    
 })
