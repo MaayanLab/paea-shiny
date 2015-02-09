8a50429f4381fa1fb0517f2a52746c42ac468d7f
zero323 <matthew.szymkiewicz@gmail.com>
2015-02-09 22:55:52 +0100

    Add basic tests for paea functions

--- tests/testthat/test_paea.R
+++ tests/testthat/test_paea.R
@@ -1,15 +1,31 @@
 library(testthat)
 
 testthat::test_that('prepare_paea_results', {
-
+    paea <- GeoDE::PAEAAnalysis(example_chdirresults, example_gmtfile)
+    description <- extract_description(preprocess_data(example_microtask_data))
+    testthat::expect_equal(
+        nrow(prepare_paea_results(paea$p_values, description)),
+        length(paea$p_values)
+    )
 })
 
 
 testthat::test_that('paea_analysis_wrapper', {
-
+    testthat::expect_identical(
+        paea_analysis_wrapper(example_chdirresults, example_gmtfile),
+        GeoDE::PAEAAnalysis(example_chdirresults, example_gmtfile)
+    )
 })
 
 
 testthat::test_that('paea_analysis_dispatch', {
+    expect_equal(
+        paea_analysis_dispatch(example_chdirresults, example_gmtfile)$down,
+        GeoDE::PAEAAnalysis(example_chdirresults_down, example_gmtfile_down)
+    )
     
+    expect_equal(
+        paea_analysis_dispatch(example_chdirresults, example_gmtfile)$up,
+        GeoDE::PAEAAnalysis(example_chdirresults_up, example_gmtfile_up)
+    )
 })
