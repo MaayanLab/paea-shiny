299ba6c45713979d290d7ffecb43fa4381c1a1b7
zero323 <matthew.szymkiewicz@gmail.com>
2015-02-28 04:16:21 +0100

    Add process_row for AllDiseases

--- geo_luigi/geo_luigi.py
+++ geo_luigi/geo_luigi.py
@@ -1,3 +1,4 @@
+import csv
 import luigi
 import rpy2.robjects.packages as rpackages
 workflows = rpackages.importr('geoWorkflows')
@@ -6,22 +7,50 @@ class GetGEO(luigi.Task):
     '''Download GEO file 
     '''
     geo_id = luigi.Parameter()
-    destdir = luigi.Parameter(None)
-    outputdir = luigi.Parameter(None) 
+    destdir = luigi.Parameter()
+    outputdir = luigi.Parameter() 
+    control = luigi.Parameter()
+    treatment = luigi.Parameter()
+    description = luigi.Parameter()
     
     def output(self):
         return [luigi.LocalTarget(_) for _ in workflows.process_gse(
                 geo_id=self.geo_id,
                 destdir=self.destdir,
-                outputdir=self.outputdir)]
+                outputdir=self.outputdir,
+                control=list(self.control),
+                treatment=list(self.treatment),
+                description=self.description
+        )]
 
     def run(self):
         self.output()
 
 
-class AllReports(luigi.Task):
+class AllDiseases(luigi.Task):
+    input_path = luigi.Parameter()
+    destdir = luigi.Parameter()
+    outputdir = luigi.Parameter() 
+ 
+    fieldnames = ('geo_id', 'disease', 'ctrl_ids', 'pert_ids', 'platform', 'cell_type')
+
+
+    def process_row(self, row):
+        ctrl_ids = row['ctrl_ids'].split(',')
+        pert_ids = row['pert_ids'].split(',')
+        geo_id = row['geo_id']
+        desc = '{0}\t{1}'.format(row['disease'], row['cell_type'])
+
+        if len(ctrl_ids) > 1 and len(pert_ids) > 1:
+            return GetGEO(geo_id, self.destdir, self.outputdir, ctrl_ids, pert_ids, desc)
+
+
     def requires(self):
-        return GetGEO('GSE7596', '.', '.')
+        with open(self.input_path) as fr:
+            reader = csv.DictReader(fr, delimiter='\t', fieldnames=self.fieldnames)
+            rows = [_ for _ in reader]
+
+        return [self.process_row(row) for row in rows]
 
 
 if __name__ == '__main__':
