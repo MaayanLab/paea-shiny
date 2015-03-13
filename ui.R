f1cca24a780f99ccf9f8638fc5efd3062cb8fe6a
zero323 <matthew.szymkiewicz@gmail.com>
2015-03-13 17:12:24 +0100

    Add shiny namespace

--- ui.R
+++ ui.R
@@ -11,62 +11,62 @@ ggvis_bug_message <- paste(
 
 #' Input separator choice
 #'
-seperator_input <- radioButtons(
+seperator_input <- shiny::radioButtons(
     'sep', 'Separator',
     c(Comma=',', Semicolon=';', Tab='\t'),
 )
 
 
-datain_input_choice <- column(3, wellPanel(
-    radioButtons(
+datain_input_choice <- shiny::column(3, shiny::wellPanel(
+    shiny::radioButtons(
         'datain_type', 'Input type',
         c('Custom expression data'='upload', 'Disease signature'='disease')
     )
 ))
 
 
-datain_upload <- column(3, wellPanel(
+datain_upload <- column(3, shiny::wellPanel(
     h3('Expression data'),
-    uiOutput('datain_container'),
+    shiny::uiOutput('datain_container'),
     seperator_input        
 ))
 
-datain_sampleclass <- column(3, wellPanel(
+datain_sampleclass <- shiny::column(3, shiny::wellPanel(
     h3('Control samples', id='control_samples'),
-    uiOutput('sampleclass_container')
+    shiny::uiOutput('sampleclass_container')
 ))
 
-datain_preprocessing <- column(3, wellPanel(
-    h3('Preprocessing', id='datain_preprocessing'),
-    checkboxInput(inputId='log2_transform', label='log2 transform' , value = FALSE),
-    checkboxInput(inputId='quantile_normalize', label='Quantile normalize', value = FALSE),
-    checkboxInput(inputId='enable_id_filter', label='Enable id filter', value = TRUE)
+datain_preprocessing <- shiny::column(3, shiny::wellPanel(
+    shiny::h3('Preprocessing', id='datain_preprocessing'),
+    shiny::checkboxInput(inputId='log2_transform', label='log2 transform' , value = FALSE),
+    shiny::checkboxInput(inputId='quantile_normalize', label='Quantile normalize', value = FALSE),
+    shiny::checkboxInput(inputId='enable_id_filter', label='Enable id filter', value = TRUE)
 ))
 
-disease_input <- column(width=6, wellPanel(
-    selectizeInput(
+disease_input <- shiny::column(width=6, shiny::wellPanel(
+    shiny::selectizeInput(
         'disease_sigs_choices', 'Choose disease signature', 
         choices = NULL, options = list(placeholder = 'type disease name')
     ),
-    uiOutput('fetch_disease_sig_container')
+    shiny::uiOutput('fetch_disease_sig_container')
 ))
 
 
-datain_preview <- column(
+datain_preview <- shiny::column(
     width=12, 
-    h3('Input preview', id='datain_preview_header'),
-    tabsetPanel(
+    shiny::h3('Input preview', id='datain_preview_header'),
+    shiny::tabsetPanel(
         id='datain_preview',
-        tabPanel(
+        shiny::tabPanel(
             'Input data',
-            p(textOutput('upload_message')),
-            dataTableOutput('contents')
+            shiny::p(shiny::textOutput('upload_message')),
+            shiny::dataTableOutput('contents')
         ),
-        tabPanel(
+        shiny::tabPanel(
             "Plots",
-            conditionalPanel(
+            shiny::conditionalPanel(
                 condition = 'output.show_datain_results === true',
-                helpText(ggvis_bug_message),
+                shiny::helpText(ggvis_bug_message),
                 ggvis::ggvisOutput('datain_density_ggvis')
             )
         )
@@ -76,17 +76,17 @@ datain_preview <- column(
 
 #' Dataset download 
 #'
-upload_tab <- tabPanel(
+upload_tab <- shiny::tabPanel(
     'Upload dataset',
-    fluidRow(
-        column(12, p('')),
+    shiny::fluidRow(
+        shiny::column(12, p('')),
         datain_input_choice,
-        conditionalPanel(
+        shiny::conditionalPanel(
             'input.datain_type === "upload"',
             datain_upload,
             datain_sampleclass
         ),
-        conditionalPanel(
+        shiny::conditionalPanel(
             'input.datain_type === "disease"',
             disease_input
         ),
@@ -97,75 +97,75 @@ upload_tab <- tabPanel(
 
 #' Characteristic Direction Analysis ouput tab
 #'
-chdir_tab <- tabPanel(
+chdir_tab <- shiny::tabPanel(
     'Characteristic Direction Analysis',
-    fluidRow(
-        column(12, p('')), 
-        column(4, wellPanel(
-            h3('Input summary', id='chdir_input_summary'),
-            tags$dl(
-                tags$dt('#genes:'),
-                tags$dd(textOutput('ngenes')),
-                tags$dt('#probes:'),
-                tags$dd(textOutput('nprobes')),
-                tags$dt('Control samples:'),
-                tags$dd(textOutput('control_samples')),
-                tags$dt('Treatment samples:'),
-                tags$dd(textOutput('treatment_samples'))
+    shiny::fluidRow(
+        shiny::column(12, shiny::p('')), 
+        shiny::column(4, shiny::wellPanel(
+            shiny::h3('Input summary', id='chdir_input_summary'),
+            shiny::tags$dl(
+                shiny::tags$dt('#genes:'),
+                shiny::tags$dd(shiny::textOutput('ngenes')),
+                shiny::tags$dt('#probes:'),
+                shiny::tags$dd(shiny::textOutput('nprobes')),
+                shiny::tags$dt('Control samples:'),
+                shiny::tags$dd(shiny::textOutput('control_samples')),
+                shiny::tags$dt('Treatment samples:'),
+                shiny::tags$dd(shiny::textOutput('treatment_samples'))
             )
         )),
    
-        column(4, wellPanel(
-            h3('CHDIR parameters', id='chdir_parameters'),
-            sliderInput('chdir_gamma', 'Gamma', 1.0, min = 0, max = 1, step = 0.05),
-            sliderInput('chdir_nnull', 'Nnull', 10, min = 2, max = 100, step = 1, round=TRUE),
-            uiOutput('random_seed_container'),
-            checkboxInput('set_random_seed', "Set RNG seed manually", FALSE),
-            helpText(paste(
+        shiny::column(4, shiny::wellPanel(
+            shiny::h3('CHDIR parameters', id='chdir_parameters'),
+            shiny::sliderInput('chdir_gamma', 'Gamma', 1.0, min = 0, max = 1, step = 0.05),
+            shiny::sliderInput('chdir_nnull', 'Nnull', 10, min = 2, max = 100, step = 1, round=TRUE),
+            shiny::uiOutput('random_seed_container'),
+            shiny::checkboxInput('set_random_seed', "Set RNG seed manually", FALSE),
+            shiny::helpText(paste(
                 'Significance test is using random number generator.',
                 'If you want to obtain reproducible results you can set',
                 'seed value.'
             )),
-            uiOutput('run_chdir_container')
+            shiny::uiOutput('run_chdir_container')
         )),
         
-        column(4,
-            wellPanel(
-                h3('Downloads', id='chdir_downloads'),
-                uiOutput('ngenes_tokeep_contatiner'),
-                tags$dl(
-                    tags$dt('#{significant upregulated genes}:'),
-                    tags$dd(textOutput('n_sig_up_genes')),
-                    tags$dt('#{significant downregulated genes}:'),
-                    tags$dd(textOutput('n_sig_down_genes'))
+        shiny::column(4,
+            shiny::wellPanel(
+                shiny::h3('Downloads', id='chdir_downloads'),
+                shiny::uiOutput('ngenes_tokeep_contatiner'),
+                shiny::tags$dl(
+                    shiny::tags$dt('#{significant upregulated genes}:'),
+                    shiny::tags$dd(shiny::textOutput('n_sig_up_genes')),
+                    shiny::tags$dt('#{significant downregulated genes}:'),
+                    shiny::tags$dd(shiny::textOutput('n_sig_down_genes'))
                 ),
-                uiOutput('chdir_downloads_container')
+                shiny::uiOutput('chdir_downloads_container')
             ),
-            wellPanel(
-                h3('Analyze with Enrichr'),
-                radioButtons('enrichr_subset', 'Subset', c('Upregulated'='up', 'Downregulated'='down')),
-                uiOutput('enrichr_form')
+            shiny::wellPanel(
+                shiny::h3('Analyze with Enrichr'),
+                shiny::radioButtons('enrichr_subset', 'Subset', c('Upregulated'='up', 'Downregulated'='down')),
+                shiny::uiOutput('enrichr_form')
             )
         ),
-        column(12,
-            h3('CHDIR results', id='chdir_results_header'),
-            tabsetPanel(
+        shiny::column(12,
+            shiny::h3('CHDIR results', id='chdir_results_header'),
+            shiny::tabsetPanel(
                 id='chdir_results',
-                tabPanel('Summary', p(textOutput('chdir_message')), uiOutput('chdir_run_summary')),
-                tabPanel('Plots',
-                    conditionalPanel(
+                shiny::tabPanel('Summary', shiny::p(shiny::textOutput('chdir_message')), shiny::uiOutput('chdir_run_summary')),
+                shiny::tabPanel('Plots',
+                    shiny::conditionalPanel(
                         condition = 'output.show_chdir_results === true',
-                        helpText(ggvis_bug_message),
+                        shiny::helpText(ggvis_bug_message),
                         ggvis::ggvisOutput('chdir_ggvis_plot')
                     ) 
                 ),
-                tabPanel(
+                shiny::tabPanel(
                     'Upregulated genes',
-                    dataTableOutput('chdir_up_genes_table')
+                    shiny::dataTableOutput('chdir_up_genes_table')
                 ),
-                tabPanel(
+                shiny::tabPanel(
                     'Downregulated genes',
-                    dataTableOutput('chdir_down_genes_table')
+                    shiny::dataTableOutput('chdir_down_genes_table')
                 )
             )
         )
@@ -175,7 +175,7 @@ chdir_tab <- tabPanel(
 
 #' Principle Angle Enrichment Analysis startegy input
 #'
-paea_strategy_radio <- radioButtons('paea_strategy', 'View:', c(
+paea_strategy_radio <- shiny::radioButtons('paea_strategy', 'View:', c(
     'in↑ ∩ lib↑' = '+up_up',
     'in↓ ∩ lib↓' = '+down_down',
     'in↑ ∩ lib↓' = '+up_down',
@@ -189,49 +189,49 @@ paea_strategy_radio <- radioButtons('paea_strategy', 'View:', c(
 
 #' paea tab - input parameters
 #'
-paea_input_parameters <-column(6, wellPanel(
-    h3('PAEA parameters', id='paea_parameters'),
-    checkboxInput('paea_casesensitive', 'Case Sensitive', FALSE),
-    helpText('Check if you want gene lables comparisons to be case sensitive. Not recommended.'),
-    uiOutput('background_dataset_container'),
-    uiOutput('run_paea_container')
+paea_input_parameters <- shiny::column(6, shiny::wellPanel(
+    shiny::h3('PAEA parameters', id='paea_parameters'),
+    shiny::checkboxInput('paea_casesensitive', 'Case Sensitive', FALSE),
+    shiny::helpText('Check if you want gene lables comparisons to be case sensitive. Not recommended.'),
+    shiny::uiOutput('background_dataset_container'),
+    shiny::uiOutput('run_paea_container')
 ))
 
 #' paea tab - downloads
 #'
-paea_downloads <- column(width=6, wellPanel(
-    h3('Downloads', id='paea_downloads'),
-    uiOutput('paea_downloads_container')
+paea_downloads <- shiny::column(width=6, shiny::wellPanel(
+    shiny::h3('Downloads', id='paea_downloads'),
+    shiny::uiOutput('paea_downloads_container')
 ))
 
 #' paea tab - paea summary
 #'
-paea_summary <- tabPanel(
-    'Summary', column(12, p(textOutput('paea_message')), uiOutput('paea_run_summary'))
+paea_summary <- shiny::tabPanel(
+    'Summary', shiny::column(12, shiny::p(shiny::textOutput('paea_message')), shiny::uiOutput('paea_run_summary'))
 )
 
 #' paea tab - output
 #'
-paea_output <- column(
+paea_output <- shiny::column(
     width=12,
-    h3('PAEA results', id='pae_results'),
+    shiny::h3('PAEA results', id='pae_results'),
     
-    tabsetPanel(
+    shiny::tabsetPanel(
         paea_summary,
-        tabPanel(
+        shiny::tabPanel(
             'Enriched sets',
-            column(
+            shiny::column(
                 width=12,
-                p(),
-                column(
+                shiny::p(),
+                shiny::column(
                     width=12,
-                    wellPanel(
+                    shiny::wellPanel(
                         paea_strategy_radio,
-                        imageOutput('paea_strategy_chart',  width = '100%', height='100%')
+                        shiny::imageOutput('paea_strategy_chart',  width = '100%', height='100%')
                     )
                 )
             ),
-            dataTableOutput('paea_results_table')
+            shiny::dataTableOutput('paea_results_table')
         )
     )
 )
@@ -239,10 +239,10 @@ paea_output <- column(
 
 #' Principle Angle Enrichment Analysis ouput tab
 #'
-paea_tab <- tabPanel(
+paea_tab <- shiny::tabPanel(
     'Principle Angle Enrichment Analysis',
-    fluidRow(
-        column(12, p('')),
+    shiny::fluidRow(
+        shiny::column(12, shiny::p('')),
         paea_input_parameters,
         paea_downloads,
         paea_output
@@ -252,9 +252,9 @@ paea_tab <- tabPanel(
 
 #' Data analysis tab
 #'
-analyze_panel <- tabPanel(
+analyze_panel <- shiny::tabPanel(
     title='Analyze',
-    tabsetPanel(
+    shiny::tabsetPanel(
         id='workflow_panel',
         upload_tab,
         chdir_tab,
@@ -264,13 +264,13 @@ analyze_panel <- tabPanel(
 
 #' About tab
 #'
-about_panel <- tabPanel(
+about_panel <- shiny::tabPanel(
     title = 'About',
-    fluidRow(column(12,
-        includeMarkdown('about.md'),
-        tags$dl(
-            tags$dt('Last update:'),
-            tags$dd(textOutput('last_modified'))
+    shiny::fluidRow(shiny::column(12,
+        shiny::includeMarkdown('about.md'),
+        shiny::tags$dl(
+            shiny::tags$dt('Last update:'),
+            shiny::tags$dd(shiny::textOutput('last_modified'))
         )
     ))
 )
@@ -278,20 +278,20 @@ about_panel <- tabPanel(
 
 #' Complete UI
 #'
-shinyUI(
-    navbarPage(
+shiny::shinyUI(
+    shiny::navbarPage(
         title='NASB Microtask Viewer',
-        footer=column(12),
+        footer=shiny::column(12),
         analyze_panel,
         about_panel,
         
-        tags$head(
-            includeCSS('www/css/tourist.css'),
-            includeCSS('www/css/custom.css'),
-            tags$script(src='js/underscore-min.js'),
-            tags$script(src='js/backbone-min.js'),
-            includeScript('www/js/tourist.min.js'),
-            includeScript('www/js/analyze-tour.js')
+        shiny::tags$head(
+            shiny::includeCSS('www/css/tourist.css'),
+            shiny::includeCSS('www/css/custom.css'),
+            shiny::tags$script(src='js/underscore-min.js'),
+            shiny::tags$script(src='js/backbone-min.js'),
+            shiny::includeScript('www/js/tourist.min.js'),
+            shiny::includeScript('www/js/analyze-tour.js')
         )
     )
 )
