a317ab51589bed8f654b589e65cc5c5c6286499d
zero323 <matthew.szymkiewicz@gmail.com>
2015-03-15 11:54:04 +0100

    Cleanup chdir ui code

--- ui.R
+++ ui.R
@@ -95,80 +95,101 @@ datain_tab <- shiny::tabPanel(
     )
 )
 
+
+#' Characteristic Direction Analysis input summary
+#'
+chdir_input_summary_col <- shiny::column(width=4, shiny::wellPanel(
+    shiny::h3('Input summary', id='chdir_input_summary'),
+    shiny::tags$dl(
+        shiny::tags$dt('#genes:'),
+        shiny::tags$dd(shiny::textOutput('ngenes')),
+        shiny::tags$dt('#probes:'),
+        shiny::tags$dd(shiny::textOutput('nprobes')),
+        shiny::tags$dt('Control samples:'),
+        shiny::tags$dd(shiny::textOutput('control_samples')),
+        shiny::tags$dt('Treatment samples:'),
+        shiny::tags$dd(shiny::textOutput('treatment_samples'))
+    )
+))
+
+
+#' Characteristic Direction Analysis parameters
+#' 
+chdir_parameters_col <- shiny::column(width=4, shiny::wellPanel(
+    shiny::h3('CHDIR parameters', id='chdir_parameters'),
+    shiny::sliderInput('chdir_gamma', 'Gamma', 1.0, min = 0, max = 1, step = 0.05),
+    shiny::sliderInput('chdir_nnull', 'Nnull', 10, min = 2, max = 100, step = 1, round=TRUE),
+    shiny::uiOutput('random_seed_container'),
+    shiny::checkboxInput('set_random_seed', "Set RNG seed manually", FALSE),
+    shiny::helpText(paste(
+        'Significance test is using random number generator.',
+        'If you want to obtain reproducible results you can set',
+        'seed value.'
+    )),
+    shiny::uiOutput('run_chdir_container')
+))
+
+
+#' Characteristic Direction Analysis results
+#' 
+chdir_results_col <- shiny::column(
+    width=12,
+    shiny::h3('CHDIR results', id='chdir_results_header'),
+    shiny::tabsetPanel(
+        id='chdir_results',
+        shiny::tabPanel('Summary', shiny::p(shiny::textOutput('chdir_message')), shiny::uiOutput('chdir_run_summary')),
+        shiny::tabPanel('Plots',
+                        shiny::conditionalPanel(
+                            condition = 'output.show_chdir_results === true',
+                            shiny::helpText(ggvis_bug_message),
+                            ggvis::ggvisOutput('chdir_ggvis_plot')
+                        ) 
+        ),
+        shiny::tabPanel(
+            'Upregulated genes',
+            shiny::dataTableOutput('chdir_up_genes_table')
+        ),
+        shiny::tabPanel(
+            'Downregulated genes',
+            shiny::dataTableOutput('chdir_down_genes_table')
+        )
+    )
+)
+
+
+#' Characteristic Direction Analysis downloads
+#' 
+chdir_downloads_col <- shiny::column(
+    width=4,
+    shiny::wellPanel(
+        shiny::h3('Downloads', id='chdir_downloads'),
+        shiny::uiOutput('ngenes_tokeep_contatiner'),
+        shiny::tags$dl(
+            shiny::tags$dt('#{significant upregulated genes}:'),
+            shiny::tags$dd(shiny::textOutput('n_sig_up_genes')),
+            shiny::tags$dt('#{significant downregulated genes}:'),
+            shiny::tags$dd(shiny::textOutput('n_sig_down_genes'))
+        ),
+        shiny::uiOutput('chdir_downloads_container')
+    ),
+    shiny::wellPanel(
+        shiny::h3('Analyze with Enrichr'),
+        shiny::radioButtons('enrichr_subset', 'Subset', c('Upregulated'='up', 'Downregulated'='down')),
+        shiny::uiOutput('enrichr_form')
+    )
+)
+
+
 #' Characteristic Direction Analysis ouput tab
 #'
 chdir_tab <- shiny::tabPanel(
     'Characteristic Direction Analysis',
     shiny::fluidRow(
         shiny::column(width=12, shiny::p('')), 
-        shiny::column(width=4, shiny::wellPanel(
-            shiny::h3('Input summary', id='chdir_input_summary'),
-            shiny::tags$dl(
-                shiny::tags$dt('#genes:'),
-                shiny::tags$dd(shiny::textOutput('ngenes')),
-                shiny::tags$dt('#probes:'),
-                shiny::tags$dd(shiny::textOutput('nprobes')),
-                shiny::tags$dt('Control samples:'),
-                shiny::tags$dd(shiny::textOutput('control_samples')),
-                shiny::tags$dt('Treatment samples:'),
-                shiny::tags$dd(shiny::textOutput('treatment_samples'))
-            )
-        )),
-   
-        shiny::column(width=4, shiny::wellPanel(
-            shiny::h3('CHDIR parameters', id='chdir_parameters'),
-            shiny::sliderInput('chdir_gamma', 'Gamma', 1.0, min = 0, max = 1, step = 0.05),
-            shiny::sliderInput('chdir_nnull', 'Nnull', 10, min = 2, max = 100, step = 1, round=TRUE),
-            shiny::uiOutput('random_seed_container'),
-            shiny::checkboxInput('set_random_seed', "Set RNG seed manually", FALSE),
-            shiny::helpText(paste(
-                'Significance test is using random number generator.',
-                'If you want to obtain reproducible results you can set',
-                'seed value.'
-            )),
-            shiny::uiOutput('run_chdir_container')
-        )),
-        
-        shiny::column(width=4,
-            shiny::wellPanel(
-                shiny::h3('Downloads', id='chdir_downloads'),
-                shiny::uiOutput('ngenes_tokeep_contatiner'),
-                shiny::tags$dl(
-                    shiny::tags$dt('#{significant upregulated genes}:'),
-                    shiny::tags$dd(shiny::textOutput('n_sig_up_genes')),
-                    shiny::tags$dt('#{significant downregulated genes}:'),
-                    shiny::tags$dd(shiny::textOutput('n_sig_down_genes'))
-                ),
-                shiny::uiOutput('chdir_downloads_container')
-            ),
-            shiny::wellPanel(
-                shiny::h3('Analyze with Enrichr'),
-                shiny::radioButtons('enrichr_subset', 'Subset', c('Upregulated'='up', 'Downregulated'='down')),
-                shiny::uiOutput('enrichr_form')
-            )
-        ),
-        shiny::column(width=12,
-            shiny::h3('CHDIR results', id='chdir_results_header'),
-            shiny::tabsetPanel(
-                id='chdir_results',
-                shiny::tabPanel('Summary', shiny::p(shiny::textOutput('chdir_message')), shiny::uiOutput('chdir_run_summary')),
-                shiny::tabPanel('Plots',
-                    shiny::conditionalPanel(
-                        condition = 'output.show_chdir_results === true',
-                        shiny::helpText(ggvis_bug_message),
-                        ggvis::ggvisOutput('chdir_ggvis_plot')
-                    ) 
-                ),
-                shiny::tabPanel(
-                    'Upregulated genes',
-                    shiny::dataTableOutput('chdir_up_genes_table')
-                ),
-                shiny::tabPanel(
-                    'Downregulated genes',
-                    shiny::dataTableOutput('chdir_down_genes_table')
-                )
-            )
-        )
+        chdir_input_summary_col,
+        chdir_parameters_col,
+        chdir_downloads_col,
+        chdir_results_col
     )
 )
 
