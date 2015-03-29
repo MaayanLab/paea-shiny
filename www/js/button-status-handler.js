413ff2a030b5123076c2cc75e185fa1d29206117
zero323 <matthew.szymkiewicz@gmail.com>
2015-03-29 23:29:12 +0200

    Style cleanup

--- www/js/button-status-handler.js
+++ www/js/button-status-handler.js
@@ -1,6 +1,6 @@
 // Change status of the action button depending on the message
 
-Shiny.addCustomMessageHandler("button_status_message",
+Shiny.addCustomMessageHandler('button_status_message',
     function(message) {
         if(message.disable) {
             $(message.id).prop( 'disabled', 'true');
