b973b02eb3db3c996ae858c2f878a35b108ab72a
zero323 <matthew.szymkiewicz@gmail.com>
2015-04-15 20:34:17 +0200

    Add test_can_load_chdir

--- test.py
+++ test.py
@@ -4,16 +4,39 @@ import os
 
 URL = os.environ['VIEWER_URL']
 
+
 class BasicWorkflowTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(2)
 
     def test_can_load(self):
         self.browser.get(URL)
         self.assertEqual(
             'NASB Microtask Viewer',
             self.browser.title
+        )
+
+    def test_can_load_chdir(self):
+        self.browser.get(URL)
+        (
+            self.browser
+                .find_element_by_id('workflow_panel')
+                .find_element_by_link_text('Characteristic Direction Analysis')
+                .click()
+        )
+
+        self.assertTrue(
+            'Upload your dataset' in self.browser
+            .find_element_by_id('run_chdir_help')
+            .text
+        )
+
+        self.assertTrue(
+            'No data' in self.browser
+            .find_element_by_id('chdir_downloads_message')
+            .text
         )
 
     def tearDown(self):
