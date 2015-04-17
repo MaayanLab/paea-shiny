55a5c029dea6cfddc72805dd2e879831c8d92d91
zero323 <matthew.szymkiewicz@gmail.com>
2015-04-17 23:58:47 +0200

    Add navbar_tabs

--- R/config.R
+++ R/config.R
@@ -14,5 +14,11 @@ config <- list(
     id_filter = 'control|chr([0-9]+|[xy]):[0-9]+-[0-9]+|//',
     sigs_path = 'http://static.zero323.net/disease-signatures/',
     sigs_list_path = 'data/disease_signatures.csv',
-    maxRequestSize = 120*1024^2
+    maxRequestSize = 120*1024^2,
+    navbar_tabs = list(
+       list(name="Home", href='/'),
+       list(name="Analyze", href='/analyze/'),
+       list(name="About", href='/about/')
+    )
+
 )
